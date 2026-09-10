d2a89f0c1c1361bc5060eddf423d640a6065f91b
Arseni Solovjov <arseni.solovjov@ivkhk.ee>
2026-09-10 10:48:29 +0300

    firebase

--- Riigid/server.js
+++ Riigid/server.js
@@ -179,6 +179,18 @@ async function startServer() {
     } catch (err) {
         console.error("Критическая ошибка запуска сервера:", err.message);
     }
+
+    const admin = require('firebase-admin');
+
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: process.env.FIREBASE_DATABASE_URL
+    });
+
+    const db = admin.database();
 }
 
 startServer();
